86a7de0e4e1d3da62f3503762e7a31dffc1ea9e2
nakamurasuka <zhongchunmingri22@gmail.com>
2024-03-24 02:20:46 +0900

    add 投稿作成処理実行

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -20,7 +20,16 @@ class PostController extends Controller
     }
     public function create()
     {
-        return view('posts/create');
+        return view('posts.create');
     }
+    
+    public function store(Request $request, Post $post)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+        
+    }
+    
 }
 ?>
